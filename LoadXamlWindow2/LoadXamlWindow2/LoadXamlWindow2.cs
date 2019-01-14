9731d5f5f3fdbc979d7a673fea7f5fe925bdc4f1
Insu Jang <mdruffy@nate.com>
2019-01-14 15:05:48 +0900

    WPF

---
+++ LoadXamlWindow2/LoadXamlWindow2/LoadXamlWindow2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace LoadXamlWindow2
+{
+    class LoadXamlWindow2
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+
+            Uri uri = new Uri("pack://application:,,,/XamlWindow.xml");
+            Stream stream = Application.GetResourceStream(uri).Stream;
+            Window win = XamlReader.Load(stream) as Window;
+
+            win.AddHandler(Button.ClickEvent, new RoutedEventHandler(Button_Click1));
+            Button b = (Button)win.FindName("XamlButton");
+            b.Click += Button_Click2;
+            app.Run(win);
+        }
+
+        static void Button_Click1(object sender, RoutedEventArgs args)
+        {
+            MessageBox.Show((args.Source as Button).Content.ToString() + "1");
+        }
+
+        static void Button_Click2(object sender, EventArgs args)
+        {
+            MessageBox.Show(((Button)sender).Content.ToString() + "2");
+        }
+    }
+}
