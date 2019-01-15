acb9274490b1844026d95a657a0a06a9b5e521fc
Insu Jang <mdruffy@nate.com>
2019-01-15 15:23:54 +0900

    WPF

---
+++ CustomElementBinding/CustomElementBinding/MyElement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CustomElementBinding
+{
+    public class MyElement : FrameworkElement
+    {
+        public static DependencyProperty NumberProperty;
+
+        static MyElement()
+        {
+            NumberProperty = DependencyProperty.Register("Number", typeof(double), typeof(MyElement), new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender));
+        }
+
+        public double Number
+        {
+            set { SetValue(NumberProperty, value); }
+            get { return (double)GetValue(NumberProperty); }
+        }
+
+        protected override Size MeasureOverride(Size sizeAvailable)
+        {
+            return new Size(200, 50);
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            dc.DrawText(
+            new FormattedText(Number.ToString(),
+            CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Times New Roman"), 12, SystemColors.WindowTextBrush), new Point(0, 0));
+        }
+    }
+}
